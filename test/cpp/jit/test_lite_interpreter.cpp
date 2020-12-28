bfc34ab798c898443b8d9ddf0809b5e4309aeda5
Chen Lai <chenlai@fb.com>
2020-12-27 22:28:02 -0800

    [Pytorch] Add source range trace to byte code debug information
    ghstack-source-id: a9ce62a97a941483bdb9537ba6cebb0332d7aaed
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/48803

--- test/cpp/jit/test_lite_interpreter.cpp
+++ test/cpp/jit/test_lite_interpreter.cpp
@@ -441,7 +441,7 @@ TEST(LiteInterpreterTest, OneSubmoduleModuleInfo) {
   }
 
   std::unordered_set<std::string> expected_result(
-      {"top(B).forward", "top(B).A0(A).forward"});
+      {"top(B).forward{}", "top(B).A0(A).forward{self.A0.forward:}"});
   AT_ASSERT(module_debug_info_set == expected_result);
 }
 
@@ -483,7 +483,9 @@ TEST(LiteInterpreterTest, TwoSubmodulesModuleInfo) {
   }
 
   std::unordered_set<std::string> expected_result(
-      {"top(C).forward", "top(C).A0(A).forward", "top(C).B0(B).forward"});
+      {"top(C).forward{}",
+       "top(C).A0(A).forward{self.A0.forward:}",
+       "top(C).B0(B).forward{self.B0.forward:}"});
   AT_ASSERT(module_debug_info_set == expected_result);
 }
 
@@ -548,7 +550,7 @@ TEST(LiteInterpreterTest, SequentialModuleInfo) {
   //     return self.A0.forward(self.B0.forward(x))
 
   std::unordered_set<std::string> expected_result(
-      {"top(C).A0(A).forward", "top(C).B0(B).forward"});
+      {"top(C).A0(A).forward{}", "top(C).B0(B).forward{self.B0.forward:}"});
   AT_ASSERT(module_debug_info_set == expected_result);
 }
 
@@ -594,9 +596,9 @@ TEST(LiteInterpreterTest, HierarchyModuleInfo) {
   // "top(C).B0(B).forward": for the add operator in B0.
   // "top(C).B0(B).forward.A0(A).forward": for the add operator in A0.
   std::unordered_set<std::string> expected_result(
-      {"top(C).forward",
-       "top(C).B0(B).forward",
-       "top(C).B0(B).forward.A0(A).forward"});
+      {"top(C).forward{}",
+       "top(C).B0(B).forward{self.B0.forward:}",
+       "top(C).B0(B).forward.A0(A).forward{self.B0.forward:self.A0.forward:}"});
   AT_ASSERT(module_debug_info_set == expected_result);
 }
 
