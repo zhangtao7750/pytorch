dff21131fe9f9187a431665d1830db49ebfef7e6
lixinyu <lixinyu@devgpu175.prn2.facebook.com>
2021-01-07 21:08:04 -0800

    webdataset prototype - ReadFilesFromZipIterableDataset
    ghstack-source-id: d20a66e263a93ab4fbbb62c747e8eeaec984a82f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/49026

--- torch/utils/data/datasets/__init__.py
+++ torch/utils/data/datasets/__init__.py
@@ -4,7 +4,9 @@ from .samplerdataset import SamplerIterableDataset
 from .listdirfilesdataset import ListDirFilesIterableDataset
 from .loadfilesfromdiskdataset import LoadFilesFromDiskIterableDataset
 from .readfilesfromtardataset import ReadFilesFromTarIterableDataset
+from .readfilesfromzipdataset import ReadFilesFromZipIterableDataset
 
 __all__ = ['BatchIterableDataset', 'CollateIterableDataset', 'ListDirFilesIterableDataset',
            'LoadFilesFromDiskIterableDataset', 'SamplerIterableDataset',
-           'ListDirFilesIterableDataset', 'LoadFilesFromDiskIterableDataset', 'ReadFilesFromTarIterableDataset']
+           'ListDirFilesIterableDataset', 'LoadFilesFromDiskIterableDataset', 'ReadFilesFromTarIterableDataset',
+           'ReadFilesFromZipIterableDataset']
