be041f4915adf3d7a7600df539df0b268fee26b5
Lillian Johnson <lillianjohnson@fb.com>
2021-01-13 08:38:57 -0800

    [WIP] JIT Static Hooks: schema checking logic
    ghstack-source-id: 12282428d8546d94b4565fae7597e8a98e145032
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/49975

--- aten/src/ATen/core/jit_type.h
+++ aten/src/ATen/core/jit_type.h
@@ -2118,6 +2118,15 @@ struct TORCH_API ClassType : public NamedType {
   torch::jit::Function* findForwardHook(const std::string& name) const;
   const std::vector<torch::jit::Function*>& getForwardHooks() const;
   const std::vector<torch::jit::Function*>& getForwardPreHooks() const;
+  
+  std::string getForwardPreHookErrorMessage(int pre_hook_idx) const;
+  std::string getForwardHookErrorMessage(int hook_idx) const;
+  void checkForwardPreHookSchema(
+      int pre_hook_idx,
+      const FunctionSchema& pre_hook_schema) const;
+  void checkForwardHookSchema(
+      int hook_idx,
+      const FunctionSchema& hook_schema) const;
 
   void addMethod(torch::jit::Function* method);
   torch::jit::Function* findMethod(const std::string& name) const;
